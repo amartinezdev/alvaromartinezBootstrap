3b0a368a4e20f9495fc8b22d64f09fe3a804e361
alvarowkz <alvarowkz@gmail.com>
2025-12-03 22:12:30 +0100

    añado light mode en la hero-section

--- cambiarTema.js
+++ cambiarTema.js
@@ -1,5 +1,6 @@
 const cambiarTema = document.getElementById("cambiarTema");
 const html = document.documentElement;
+const inicio = document.querySelector("#inicio");
 
 /*
   Función que cambia el tema de la página
@@ -11,8 +12,12 @@ function changeTheme() {
   if (temaActual == "dark") {
     html.setAttribute("data-bs-theme", "light");
     cambiarTema.textContent = "☀️";
+    inicio.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
+    inicio.style.backgroundBlendMode = "darken";
   } else {
     html.setAttribute("data-bs-theme", "dark");
     cambiarTema.textContent = "🌙";
+    inicio.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+    inicio.style.backgroundBlendMode = "darken";
   }
 }
